01bcb9d31b641649600c4ab841eae3e7dfbe0ef6
awmleer <awmleer@sparker.xyz>
2017-07-17 18:12:10 +0800

    [add] router for paper-add

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -3,11 +3,15 @@ import {PapersComponent} from "../pages/papers/papers.component";
 import {LabelsComponent} from "../pages/labels/labels.component";
 import {PaperDetailComponent} from "../pages/paper-detail/paper-detail.component";
 import {PaperEditComponent} from "../pages/paper-edit/paper-edit.component";
+import {PaperAddComponent} from "../pages/paper-add/paper-add.component";
 
 export const Router=RouterModule.forRoot([
   {
     path: 'papers',
     component: PapersComponent
+  },{
+    path: 'paper/add',
+    component: PaperAddComponent,
   },{
     path: 'labels',
     component: LabelsComponent
