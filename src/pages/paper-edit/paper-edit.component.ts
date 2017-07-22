8133e39a2dc60a414855acd173d64258d42df233
awmleer <awmleer@sparker.xyz>
2017-07-22 20:34:24 +0800

    [feat] edit teachers of paper

--- src/pages/paper-edit/paper-edit.component.ts
+++ src/pages/paper-edit/paper-edit.component.ts
@@ -3,6 +3,7 @@ import {Params, ActivatedRoute} from "@angular/router";
 import {CONFIG} from "../../app/config";
 import {Http} from "@angular/http";
 import { Location } from '@angular/common';
+import {PaperDetail} from "../../classes/paper";
 
 @Component({
   selector: 'app-paper-edit',
@@ -10,32 +11,47 @@ import { Location } from '@angular/common';
   styleUrls: ['./paper-edit.component.scss']
 })
 export class PaperEditComponent implements OnInit {
-  paper=null;
+  paper:PaperDetail=null;
+  teachersText:string='';
 
 
   constructor(
     private http: Http,
     public location: Location,
     private route: ActivatedRoute,
-  ) { }
+  ) {}
 
   ngOnInit() {
     this.route.params
       .subscribe((params: Params)=>{
         this.http.get(`${CONFIG.apiUrl}/paper/${params['id']}/detail/`).toPromise().then(response=>{
           this.paper=response.json();
+          this.teachersText='';
+          for (let i in this.paper.teachers) {
+            this.teachersText+=this.paper.teachers[i].name+' ';
+          }
         });
       });
   }
 
   submit(){
+    let teachers = this.teachersText.split(' ');
+    for (let i = 0; i < teachers.length; i++) {
+      if(teachers[i]==''){
+        teachers.splice(i,1);
+        i--;
+      }
+    }
+
+    console.log(teachers);
     this.http.post(CONFIG.apiUrl+`/paper/${this.paper.id}/edit/`,{
       title:this.paper.title,
       subject:this.paper.subject,
       keyword:this.paper.keyword,
       publishYear:this.paper.publishYear,
       abstract:this.paper.abstract,
-      major:this.paper.major
+      major:this.paper.major,
+      teachers: teachers
     }).toPromise().then(response=>{
       let data = response.json();
       if (data['status']=='success') {
