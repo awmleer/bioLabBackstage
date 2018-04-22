a9e38412c01b448d983abca6ba2b02e3b95a2ff0
awmleer <awmleer@sparker.xyz>
2018-04-22 09:46:28 +0800

    update instrument edit page

--- src/classes/instrument.ts
+++ src/classes/instrument.ts
@@ -5,9 +5,20 @@ export class InstrumentEntry {
 }
 
 export class InstrumentBrief extends InstrumentEntry {
+  coverImg: string;//url
   modelNumber: string;
 }
 
 export class InstrumentDetail extends InstrumentBrief {
-  content: string;//HTML
+  content: {
+    outer: string;
+    inner: string;
+    instructions: string;
+    attentions: string;
+  } = {
+    outer: '',
+    inner: '',
+    instructions: '',
+    attentions: ''
+  };//HTML
 }
