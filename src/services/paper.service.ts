df8644c1c0300db45e8c33bf82defa164cb71575
awmleer <awmleer@sparker.xyz>
2018-04-29 19:51:55 +0800

    fix label

--- src/services/paper.service.ts
+++ src/services/paper.service.ts
@@ -2,9 +2,9 @@ import { Injectable } from '@angular/core';
 
 import 'rxjs/add/operator/toPromise';
 import {ApiService} from './api.service';
-import {PaperDetail, PaperLabel} from '../classes/paper';
+import {PaperDetail} from '../classes/paper';
 import * as _ from 'lodash';
-import {LabeledItemService} from '../classes/label';
+import {Label, LabeledItemService} from '../classes/label';
 
 
 @Injectable()
@@ -19,7 +19,7 @@ export class PaperService implements LabeledItemService {
     return this.apiSvc.get(`/paper/${id}/detail/`);
   }
 
-  removeLabel(paper:PaperDetail, label:PaperLabel):Promise<void>{
+  removeLabel(paper:PaperDetail, label:Label):Promise<void>{
     return this.apiSvc.get(`/paper/${paper.id}/label/remove/${label.id}/`).then(()=>{
       _.remove(paper.labels,{
         id:label.id
@@ -27,7 +27,7 @@ export class PaperService implements LabeledItemService {
     });
   }
 
-  addLabel(paper:PaperDetail, label:PaperLabel):Promise<void>{
+  addLabel(paper:PaperDetail, label:Label):Promise<void>{
     return this.apiSvc.get(`/paper/${paper.id}/label/add/${label.id}/`).then(()=>{
       if (_.isUndefined(_.find(paper.labels,{id:label.id}))) {
         paper.labels.push(label);
--- src/services/reagent.service.ts
+++ src/services/reagent.service.ts
@@ -2,9 +2,9 @@ import { Injectable } from '@angular/core';
 
 import 'rxjs/add/operator/toPromise';
 import {ApiService} from './api.service';
-import {ReagentDetail, ReagentLabel} from '../classes/reagent';
+import {ReagentDetail} from '../classes/reagent';
 import * as _ from 'lodash';
-import {LabeledItemService} from '../classes/label';
+import {Label, LabeledItemService} from '../classes/label';
 
 
 @Injectable()
@@ -15,7 +15,7 @@ export class ReagentService implements LabeledItemService {
     private apiSvc: ApiService,
   ) {}
 
-  removeLabel(reagent:ReagentDetail, label:ReagentLabel):Promise<void>{
+  removeLabel(reagent:ReagentDetail, label:Label):Promise<void>{
     return this.apiSvc.get(`/reagent/${reagent.id}/label/remove/${label.id}/`).then(()=>{
       _.remove(reagent.labels,{
         id:label.id
@@ -23,7 +23,7 @@ export class ReagentService implements LabeledItemService {
     });
   }
 
-  addLabel(reagent:ReagentDetail, label:ReagentLabel):Promise<void>{
+  addLabel(reagent:ReagentDetail, label:Label):Promise<void>{
     return this.apiSvc.get(`/reagent/${reagent.id}/label/add/${label.id}/`).then(()=>{
       if (_.isUndefined(_.find(reagent.labels,{id:label.id}))) {
         reagent.labels.push(label);
