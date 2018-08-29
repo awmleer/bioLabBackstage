5a8acd26b835782add31c986606e9e9c15e47e1e
awmleer <awmleer@sparker.xyz>
2018-08-30 00:20:12 +0800

    fix

--- src/pages/login/login.component.ts
+++ src/pages/login/login.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import {AccountService} from '../../services/account.service';
-import {Location} from '@angular/common';
 import {NzMessageService} from 'ng-zorro-antd';
 import {Router} from '@angular/router';
 
