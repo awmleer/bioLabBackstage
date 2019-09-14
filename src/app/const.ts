c48050ea69c93d6e1f0aa05cfbbfad32f1601546
awmleer <awmleer@sparker.xyz>
2019-09-14 23:33:57 +0800

    fix domain name

--- src/app/const.ts
+++ src/app/const.ts
@@ -3,5 +3,5 @@ import {environment} from '../environments/environment'
 export const CONST = {
   // apiUrl:'/api'
   // apiUrl:environment.production?'':'/api'
-  apiUrl:'http://49.234.197.80'
+  apiUrl:'http://biochem.sparker.xyz'
 };
